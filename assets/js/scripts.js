66b05d66b67f6d50c907cdcfb76bc87eb3a41ced
JoalisonM <joalisonmatheus10@gmail.com>
2021-09-16 23:04:58 -0300

    layout responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,23 @@ const Modal = {
     document.querySelector('.modal-overlay').classList.remove('active')
   }
 }
+
+const menuMobile = document.querySelector(".menuMobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchStart") event.preventDefault();
+  const nav = document.querySelector(".headerContent nav");
+  nav.classList.toggle('active');
+
+  const active = nav.classList.contains("active");
+
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+menuMobile.addEventListener("click", toggleMenu);
+menuMobile.addEventListener("touchStart", toggleMenu);
